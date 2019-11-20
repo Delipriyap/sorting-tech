dc50b2bdf0d9d1b4c0fc81e74309d3b9518cca3c
Delipriyap <deli27priya@gmail.com>
2019-11-20 11:02:51 +0530

    main

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 #include "head.h"
 int main()
-{
-	int a[]={9,8,7,6,5,4,32,1};
+{ 
+        int a[]={9,8,7,6,5,4,32,1};
 	int n= sizeof(a)/sizeof(a[0]);
+        int r= sizeof(a)/sizeof(a[0]);
 	Bubble(a,n);
 	Insertion(a,n);
 	Selection(a,n);
+        Quick(a,0,n);
+        Merge(a,0,r);
 	return 0;
 }
