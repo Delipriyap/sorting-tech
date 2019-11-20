72d7893fc8f19f4a1de69976a1af67f960fe82c1
Delipriyap <deli27priya@gmail.com>
2019-11-20 11:12:52 +0530

    head.h

--- head.h
+++ head.h
@@ -1,4 +1,6 @@
 extern void Bubble(int a[],int n);
 extern void Insertion(int a[],int n);
 extern void Selection(int a[],int n);
+extern void Quick(int a[],int low,int n);
+extern void Merge(int a[],int l,int r);
 
